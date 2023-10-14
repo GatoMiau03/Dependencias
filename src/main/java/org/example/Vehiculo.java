d89f30a056deb3b8c4c4db0782af0108ac90f0aa
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-10-14 17:00:29 -0300

    aa

--- src/main/java/org/example/Vehiculo.java
+++ src/main/java/org/example/Vehiculo.java
@@ -2,7 +2,7 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Persona {
+public class Vehiculo {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
