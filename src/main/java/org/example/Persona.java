400f40aded6832b88bd27c2ac5611ce71386e155
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-10-14 18:41:07 -0300

    Este es el codigo creado a partir del diagrama del VPP

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,19 +1,35 @@
-package org.example;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Persona {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+	private string nombre;
+	private string destino;
+	private Vehiculo vehiculo;
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
+	/**
+	 * 
+	 * @param nombre
+	 * @param destino
+	 * @param vehiculo
+	 */
+	public Persona(string nombre, string destino, Vehiculo vehiculo) {
+		// TODO - implement Persona.Persona
+		throw new UnsupportedOperationException();
+	}
+
+	public string getNombre() {
+		return this.nombre;
+	}
+
+	public string getDestino() {
+		return this.destino;
+	}
+
+	public Vehiculo getVehiculo() {
+		return this.vehiculo;
+	}
+
+	public int calcularTiempo() {
+		// TODO - implement Persona.calcularTiempo
+		throw new UnsupportedOperationException();
+	}
+
 }
