b2ef9c55b1ba6588f3419cbc9bcc2421c3e30550
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-10-14 22:35:44 -0300

    9

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,19 +1,29 @@
 package org.example;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Persona {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private String nombre;
+    private String destino;
+    private Vehiculo vehiculo;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public Persona(String nombre, String destino, Vehiculo vehiculo) {
+        this.nombre = nombre;
+        this.destino = destino;
+        this.vehiculo = vehiculo;
+    }
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getDestino() {
+        return this.destino;
+    }
+
+    public Vehiculo getVehiculo() {
+        return this.vehiculo;
+    }
+
+    public int calcularTiempo() {
+        // Utiliza el tiempo de viaje del vehículo elegido para calcular el tiempo de viaje.
+        return this.vehiculo.getTiempoViaje();
     }
 }
